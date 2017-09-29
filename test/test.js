c0009f09adb79df3c8f2ff07885af72898d9de14
Jim Toth <jim@meme.coach>
2017-09-29 04:14:06 -0400

    Cleaning up test.js

--- test/test.js
+++ test/test.js
@@ -1,18 +1,19 @@
 'use strict';
 require('./support/setup.js');
-const expect = require('chai').expect;
-const should = require('chai').should();
-const sinon = require('sinon');
-const nock = require('nock');
-const Probe = require('../src/km.probe');
+var expect = require('chai').expect,
+    should = require('chai').should(),
+    sinon  = require('sinon'),
+    nock   = require('nock'),
+    Probe  = require('../src/km.probe');
 
-const ENDPOINT = 'http://mock.endpoint';
-const SECURE_ENDPOINT = 'https://mock.endpoint';
-const MOCK_PAYLOAD = [{ foo: 'bar' }];
+const ENDPOINT        = 'http://mock.endpoint',
+      SECURE_ENDPOINT = 'https://mock.endpoint',
+      MOCK_PAYLOAD    = [{ foo: 'bar' }];
 
 var nock_http, nock_https;
 
 describe('km.probe', function () {
+
   before(function () {
     nock_http = nock(ENDPOINT)
       .persist()
@@ -23,32 +24,42 @@ describe('km.probe', function () {
       .get('/')
       .reply(200, MOCK_PAYLOAD);
   });
+
   it('is constructed with an endpoint', function () {
     let probe = new Probe(ENDPOINT);
     return probe.endpoint.should.equal(ENDPOINT);
   });
+
   it('throws a TypeError when endpoint is not a string', function () {
     let emptyConstructor = () => { new Probe() };
     return emptyConstructor.should.throw(TypeError);
   });
+
   it('.deploy() returns a Promise', function () {
     let probe = new Probe(ENDPOINT);
     return probe.deploy().should.be.a('promise');
   });
+
   describe('can make requests via', function () {
+
     it('http', function () {
       let probe = new Probe(ENDPOINT);
       return probe.deploy().should.be.fulfilled;
     });
+
     it('https', function () {
       let probe = new Probe(SECURE_ENDPOINT);
       return probe.deploy().should.be.fulfilled;
     });
+
   });
+
   describe('formats resulting payload data', function () {
+
     it('raw, as-is (default)', function () {
       let probe = new Probe(ENDPOINT);
       return probe.deploy().should.become(MOCK_PAYLOAD);
     });
+
   });
 });
