e189fa3d16980f0a8de5ae311b71ac6df7aeb46b
Jim Toth <jim@meme.coach>
2017-09-29 08:43:47 -0400

    Added custom output formatting.  Added ability for raw payload to be added to output. Added simple and complex sample JSON payloads sets.

--- src/km.probe.js
+++ src/km.probe.js
@@ -3,11 +3,13 @@
 var needle = require('needle');
 
 class Probe {
-  constructor(endpoint) {
+  constructor(endpoint, format, raw) {
     if (typeof endpoint !== 'string') {
       throw new TypeError(`Endpoint must be a string. Got ${endpoint}`);
     }
     this.endpoint = endpoint;
+    this.format = format;
+    this.raw = raw;
   }
 
   deploy() {
@@ -21,10 +23,36 @@ class Probe {
   _resolve(resolve, reject) {
     return (error, response) => {
       if (!error && response.statusCode == 200) {
-        resolve(response.body);
+        if (this.format) {
+          this._format(response.body, resolve, reject);
+        } else {
+          resolve(response.body);
+        }
       } else {
         reject(error);
       }
+    }
+  }
+
+  _format(data, resolve, reject) {
+    let payload = {}, key;
+    for (key in this.format) {
+      if (this.format[key] == this.raw) {
+        payload[key] = data;
+      } else {
+        payload[key] = this._deepest(this.format[key].split('.'), data);
+      }
+    }
+    resolve(payload);
+  }
+
+  _deepest(r_keys, haystack) {
+    let spliced = [];
+    if (r_keys.length == 1) {
+      return haystack[r_keys[0]];
+    } else {
+      spliced = r_keys.splice(0,1);
+      return this._deepest(r_keys, haystack[spliced[0]]);
     }
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,16 +1,20 @@
 'use strict';
+
 require('./support/setup.js');
+
 var expect = require('chai').expect,
     should = require('chai').should(),
     sinon  = require('sinon'),
     nock   = require('nock'),
     Probe  = require('../src/km.probe');
 
-const ENDPOINT        = 'http://mock.endpoint',
-      SECURE_ENDPOINT = 'https://mock.endpoint',
-      MOCK_PAYLOAD    = [{ foo: 'bar' }];
+const ENDPOINT         = 'http://mock.endpoint',
+      SECURE_ENDPOINT  = 'https://mock.endpoint',
+      COMPLEX_ENDPOINT = ENDPOINT + '/complex',
+      SIMPLE_DATA      = require('./support/sample-data/simple.json'),
+      COMPLEX_DATA     = require('./support/sample-data/complex.json');
 
-var nock_http, nock_https;
+var nock_http, nock_https, nock_complex;
 
 describe('km.probe', function () {
 
@@ -18,11 +22,17 @@ describe('km.probe', function () {
     nock_http = nock(ENDPOINT)
       .persist()
       .get('/')
-      .reply(200, MOCK_PAYLOAD);
+      .reply(200, SIMPLE_DATA);
+
     nock_https = nock(SECURE_ENDPOINT)
       .persist()
       .get('/')
-      .reply(200, MOCK_PAYLOAD);
+      .reply(200, SIMPLE_DATA);
+
+    nock_complex = nock(ENDPOINT)
+      .persist()
+      .get('/complex')
+      .reply(200, COMPLEX_DATA);
   });
 
   it('is constructed with an endpoint', function () {
@@ -41,7 +51,7 @@ describe('km.probe', function () {
   });
 
   describe('can make requests via', function () {
-
+    
     it('http', function () {
       let probe = new Probe(ENDPOINT);
       return probe.deploy().should.be.fulfilled;
@@ -54,11 +64,56 @@ describe('km.probe', function () {
 
   });
 
+  it('can handle complex data', function () {
+    let probe = new Probe(COMPLEX_ENDPOINT);
+    return probe.deploy().should.become(COMPLEX_DATA);
+  });
+
   describe('formats resulting payload data', function () {
 
     it('raw, as-is (default)', function () {
       let probe = new Probe(ENDPOINT);
-      return probe.deploy().should.become(MOCK_PAYLOAD);
+      return probe.deploy().should.become(SIMPLE_DATA);
+    });
+
+    it('to an optionally specified JSON format, recursively', function () {
+      let probe = new Probe(COMPLEX_ENDPOINT, {
+        'should-be-bar': 'foo',
+        'should-be-true': 'foo-true',
+        'should-be-false': 'foo-false',
+        'should-be-7': 'foo-int',
+        'should-be-3.141519': 'foo-float',
+        'should-be-array': 'foo-array',
+        'should-be-object': 'foo-object',
+        'should-be-recursive': 'foo-recursive.recursive1.recursive2',
+        'should-be-empty-string': 'foo-empty-string',
+        'should-be-null': 'foo-null',
+        'should-be-empty-array': 'foo-empty-array',
+        'should-be-empty-object': 'foo-empty-object'
+      });
+      return probe.deploy().should.become({
+        'should-be-bar': 'bar',
+        'should-be-true': true,
+        'should-be-false': false,
+        'should-be-7': 7,
+        'should-be-3.141519': 3.141519,
+        'should-be-array': ['bar0', 'bar1', 'bar2'],
+        'should-be-object': {'foo-object-foo': 'foo-object-bar'},
+        'should-be-recursive': 'recursive3',
+        'should-be-empty-string': '',
+        'should-be-null': null,
+        'should-be-empty-array': [],
+        'should-be-empty-object': {}
+      });
+    });
+
+    it('optionally allows raw input to be exposed to a new key', function () {
+      let format = { 'should-be-bar': 'foo', 'should-be-raw-payload': 'raw' },
+          probe  = new Probe(COMPLEX_ENDPOINT, format, 'raw');
+      return probe.deploy().should.become({
+        'should-be-bar': 'bar',
+        'should-be-raw-payload': COMPLEX_DATA
+      });
     });
 
   });
