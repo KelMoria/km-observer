22ecd8fd9c4b5e8dfa36e064f223b83f66c37d66
Jim Toth <jim@meme.coach>
2017-09-29 15:29:05 -0400

    Cleaned up index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 'use strict';
 
-var Observer = require('./lib/km.observer');
-
-module.exports = Observer;
-
-var observer = new Observer('https://api.gdax.com/products/eth-usd/ticker');
+module.exports = require('./lib/km.observer');
